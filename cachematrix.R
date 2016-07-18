7ad7703f74f83beb6dee77da019f41106152ee7b
gab4 <gbisc7878@gmail.com>
2016-07-18 16:25:12 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix is a function that first empties the contents
+#of the variables 'm' and  'n' by establishing that 'm' and 'n'
+#are NULL. 
+#This makes sure that if there was any cached data in 'm' and
+#'n' prior to calling this function, that cached data is now
+#emptied from those variables used in the function.
+#makeCacheMatrix then takes the argument 'x', which is always
+#a matrix, and caches it and its inverse in the variables
+#'n' and 'm' respectively. 
 
-## Write a short comment describing this function
+#cacheSolve identifies whether the variable m has any cached 
+#data in it.
+#If m does have cached data within, the function will return m, 
+#printing all the data inside m.
+#If m does not have cached data, it will call the
+#makeCacheMatrix function on the argument 'x' provided by the 
+#cacheSolve function.
+#'x' should always contain a square, invertible matrix, otherwise
+#the function will not work.
+#Because the cacheSolve function implements the makeCacheMatrix
+#function, cacheSolve should be used in conjunction with 
+#makeCacheMatrix, after makeCacheMatrix has been defined as a
+#function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        n <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+                n <<- NULL
+        }
+        m <<- solve(x)
+        n <<- x
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+        if(!is.null(m)) {
+                return(m)
+        } else {
+                makeCacheMatrix(x)
+                print(m)
+                print(n)
+        }
 }
